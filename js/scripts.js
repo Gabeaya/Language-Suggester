057d941095239f0863435c48751920e9a269daee
Gabriel Ayala <gabeayala100@gmail.com>
2021-06-17 13:15:53 -0700

    Create a button function to show the survey.

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,9 @@
 $(document).ready(function() {
   $("form#user").submit(function(event) {
     event.preventDefault();
-  })
+  });
+  $("button#start").click(function() {
+    $(".survey-showing").show();
+  });
+  
 })
