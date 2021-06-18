06bc1143b00b038bdffa7f8388282493b0e9be60
Gabriel Ayala <gabeayala100@gmail.com>
2021-06-17 20:03:04 -0700

    Add select pathways to variables and add a submit function to the form.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,27 @@
 
 //User Interface Logic:
 $(document).ready(function() {
-  $("form#user").submit(function(event) {
+  $("button#start").click(function() {
+    $("#survey-showing").fadeIn();
+  });
+  $("form#survey").submit(function(event) {
+    
+    const projectType = $("select#project-type").val();
+    const somethingToMaster = $("select#something-to-master").val();
+    const money = $("select#money") .val();
+    
+    if (projectType === "web-developement") {
+      let matchShowing = "Javascript";
+    } else if (projectType === "web-design") {
+      let matchShowing = "C++";
+    } else if (projectType === "video-game") {
+      let matchShowing = "HTML5";
+    } else if (projectType === "desktop-software") {
+      let matchShowing = "Your mom";
+    } 
+    $("#language").text(matchShowing);
+    $("#matchShowing").show();
+
     event.preventDefault();
   });
-  $("button#start").click(function() {
-    $(".survey-showing").show();
-  });
-  $("button#complete-form").click(function() {
-    $(".match").show();
-  })
-})
+});
